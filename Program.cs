6cbe947a3ea1695d484b538ae3a3245f45e3e005
zzz711 <zzz8910@gmail.com>
2018-03-10 12:32:06 -0500

    Working matchup predictor in C#

--- Program.cs
+++ Program.cs
@@ -8,7 +8,7 @@ namespace statcompare
         private Team GetStats(string team)
         {
             var teamStats = new Team();
-            teamStats.teamName = team;
+            teamStats.TeamName = team;
             
             using (var conn =
                 new NpgsqlConnection("Server=192.168.0.5; User Id =postgres; Password=password; Database =basketball"))
@@ -28,9 +28,21 @@ namespace statcompare
                     
                     while(reader.Read())
                     {
-                        Console.WriteLine(reader.GetString(1));
-                        Console.WriteLine(reader.GetString(2));
-                        teamStats.offense = Int32.Parse(reader.GetString(0));
+                        teamStats.Offense =Double.Parse( reader.GetString(1));
+                        teamStats.Defense = Double.Parse(reader.GetString(2));
+                        teamStats.Margin = Double.Parse(reader.GetString(3));
+                        teamStats.Rebound = Double.Parse(reader.GetString(4));
+                        teamStats.Assists = Double.Parse(reader.GetString(5));
+                        teamStats.Blocks = Double.Parse(reader.GetString(6));
+                        teamStats.Steals = Double.Parse(reader.GetString(7));
+                        teamStats.Turnovers = Double.Parse(reader.GetString(8));
+                        teamStats.AssistsTurnovers = Double.Parse(reader.GetString(9));
+                        teamStats.FieldGoalPercent = Double.Parse(reader.GetString(10));
+                        teamStats.FieldGoalDefense = Double.Parse(reader.GetString(11));
+                        teamStats.ThreesPerGame = Double.Parse(reader.GetString(12));
+                        teamStats.ThreesPercent = Double.Parse(reader.GetString(13));
+                        teamStats.FreeThrowPercent = Double.Parse(reader.GetString(14));
+                        teamStats.WinLossPercent = Double.Parse(reader.GetString(15));
                     }   
                     reader.Close();
                 }
@@ -38,6 +50,138 @@ namespace statcompare
             }
 
             return teamStats;
+        }
+
+        private bool HigherIsBetter(double teamAStat, double teamBStat)
+        {
+            return teamAStat > teamBStat;
+        }
+
+        private bool LowerIsBetter(double teamAStat, double teamBStat)
+        {
+            return teamAStat < teamBStat;
+        }
+
+        private void RecordResult(string name, double score)
+        {
+            string result = String.Format("{0} {1}", name, score);
+            System.IO.File.WriteAllText("Results.txt", result);
+        }
+        
+        private void PredictWinner(Team teamOne, Team teamTwo)
+        {            
+            if (HigherIsBetter(teamOne.Offense , teamTwo.Offense))
+                teamOne.Score += 6.67;
+            else
+            {
+                teamTwo.Score += 6.67;
+            }
+
+            if (LowerIsBetter(teamOne.Defense, teamTwo.Defense))
+                teamOne.Score += 6.67;
+            else
+            {
+                teamTwo.Score += 6.67;
+            }
+
+            if (HigherIsBetter(teamOne.Margin, teamTwo.Margin))
+                teamOne.Score += 6.67;
+            else
+            {
+                teamTwo.Score += 6.67;
+            }
+
+            if (HigherIsBetter(teamOne.Rebound, teamTwo.Rebound))
+                teamOne.Score += 6.67;
+            else
+            {
+                teamTwo.Score += 6.67;
+            }
+
+            if (HigherIsBetter(teamOne.Assists, teamTwo.Assists))
+                teamOne.Score += 6.67;
+            else
+            {
+                teamTwo.Score += 6.67;
+            }
+
+            if (HigherIsBetter(teamOne.Blocks, teamTwo.Blocks))
+                teamOne.Score += 6.67;
+            else
+            {
+                teamTwo.Score += 6.67;
+            }
+
+            if (HigherIsBetter(teamOne.Steals, teamTwo.Steals))
+                teamOne.Score += 6.67;
+            else
+            {
+                teamTwo.Score += 6.67;
+            }
+
+            if (HigherIsBetter(teamOne.Turnovers, teamTwo.Turnovers))
+                teamOne.Score += 6.67;
+            else
+            {
+                teamTwo.Score += 6.67;
+            }
+
+            if (HigherIsBetter(teamOne.AssistsTurnovers, teamTwo.AssistsTurnovers))
+                teamOne.Score += 6.67;
+            else
+            {
+                teamTwo.Score += 6.67;
+            }
+
+            if (HigherIsBetter(teamOne.FieldGoalPercent, teamTwo.FieldGoalPercent))
+                teamOne.Score += 6.67;
+            else
+            {
+                teamTwo.Score += 6.67;
+            }
+
+            if (LowerIsBetter(teamOne.FieldGoalDefense, teamTwo.FieldGoalDefense))
+                teamOne.Score += 6.67;
+            else
+            {
+                teamTwo.Score += 6.67;
+            }
+
+            if (HigherIsBetter(teamOne.ThreesPerGame, teamTwo.ThreesPerGame))
+                teamOne.Score += 6.67;
+            else
+            {
+                teamTwo.Score += 6.67;
+            }
+
+            if (HigherIsBetter(teamOne.ThreesPercent, teamTwo.ThreesPercent))
+                teamOne.Score += 6.67;
+            else
+            {
+                teamTwo.Score += 6.67;
+            }
+            
+            if (HigherIsBetter(teamOne.FreeThrowPercent, teamTwo.FreeThrowPercent))
+                teamOne.Score += 6.67;
+            else
+            {
+                teamTwo.Score += 6.67;
+            }
+
+            if (HigherIsBetter(teamOne.WinLossPercent, teamTwo.WinLossPercent))
+                teamOne.Score += 6.67;
+            else
+            {
+                teamTwo.Score += 6.67;
+            }
+            
+            if(teamOne.Score > teamTwo.Score)
+                RecordResult(teamOne.TeamName, teamOne.Score);
+            else
+            {
+                RecordResult(teamTwo.TeamName, teamTwo.Score);
+            }
+
         }
         
         static int Main(string[] args)
@@ -52,30 +196,34 @@ namespace statcompare
 
             var program = new Program();
             for (var i = 0; i < teams.Length; i += 2)
-                program.GetStats(teams[i]);
+            {
+                var teamA= program.GetStats(teams[i]);
+                var teamB = program.GetStats(teams[i + 1]);
+                program.PredictWinner(teamA, teamB);
+            }
 
             return 0;
         }
     }
 
-    public class Team
+    public struct Team
     {
-        public string teamName;
-        public int offense;
-        public int defense;
-        public int margin;
-        public int rebound;
-        public int assists;
-        public int blocks;
-        public int shots;
-        public int steals;
-        public int turnovers;
-        public int assistsTurnovers;
-        public int fieldGoalPercent;
-        public int fieldGoalDefense;
-        public int threesPerGame;
-        public int threesPercent;
-        public int freeThrowPercent;
-        public int winLossPercent;
+        public string TeamName;
+        public double Offense;
+        public double Defense;
+        public double Margin;
+        public double Rebound;
+        public double Assists;
+        public double Blocks;
+        public double Steals;
+        public double Turnovers;
+        public double AssistsTurnovers;
+        public double FieldGoalPercent;
+        public double FieldGoalDefense;
+        public double ThreesPerGame;
+        public double ThreesPercent;
+        public double FreeThrowPercent;
+        public double WinLossPercent;
+        public double Score;
     }
 }
